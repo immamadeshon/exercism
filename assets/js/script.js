17dea76580cb6691bf4c30dde27d5843bffeb521
immamadeshon <mani.darvish32@gmail.com>
2025-08-01 20:25:08 +0330

    exercism step2

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// exercism hello world ...
+
+export function hello() {
+  return 'Hello, World!';
+}
+console.log(hello);
+
+// exercism two fer ...
+
+export const twoFer = name => {
+  return name ? `One for ${name}, one for me.` : `One for you, one for me.`;
+};
