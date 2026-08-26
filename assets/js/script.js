e12a15e9b664dd1a2679c07a26715fcb2c1bce70
immamadeshon <mani.darvish32@gmail.com>
2026-08-26 18:37:37 +0330

    step5 task3

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,3 +59,12 @@ export const rows = (count) => {
   
   return triangle;
 };
+
+// resistor color...
+
+export const colorCode = (color) => COLORS.indexOf(color);
+
+export const COLORS = [
+  'black', 'brown', 'red', 'orange', 'yellow','green', 'blue', 'violet', 'grey', 'white',
+];
+
